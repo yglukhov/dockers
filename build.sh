8843e94b823fd907bd2a0f168509fcb2b995ff8b
Yuriy Glukhov <yutiy.glukhov@gmail.com>
2016-05-17 19:41:31 +0300

    ok

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 
 set -ev
 
+if [ "$1" = "all" -o "$BUILD_ALL" = "true" ]
+then
+    docker build -t yglukhov/falcon-pre-nim falcon-pre-nim
+    docker push yglukhov/falcon-pre-nim
+fi
+
 docker build -t yglukhov/falcon falcon
 docker push yglukhov/falcon
