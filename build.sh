401c57569d6ec2f3d4521d026e0315a796a72b91
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2017-02-07 13:23:07 +0200

    Tag images with date

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 
 set -ev
 
+TAG=$(date "+%Y%m%d%H%M")
+
+echo "The images will be tagged as $TAG"
+
 if [ "$1" = "all" -o "$BUILD_ALL" = "true" ]
 then
-    docker build -t yglukhov/falcon-pre-nim falcon-pre-nim
-    docker push yglukhov/falcon-pre-nim
+    docker build -t yglukhov/falcon-pre-nim:$TAG falcon-pre-nim
+    docker push yglukhov/falcon-pre-nim:$TAG
 fi
 
-docker build -t yglukhov/falcon falcon
-docker push yglukhov/falcon
+docker build -t yglukhov/falcon:$TAG falcon
+docker push yglukhov/falcon:$TAG
